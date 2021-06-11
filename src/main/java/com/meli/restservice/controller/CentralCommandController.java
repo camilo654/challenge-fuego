fda2c7161ba01b7c4a1afb07cc102be18301e0ff
Christian Torres <cctorresr@unal.edu.co>
2021-06-10 22:56:47 -0500

    Se agrega función getMessage.

--- src/main/java/com/meli/restservice/controller/CentralCommandController.java
+++ src/main/java/com/meli/restservice/controller/CentralCommandController.java
@@ -1,19 +1,8 @@
 package com.meli.restservice.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.meli.restservice.service.IRadarService;
 
 @RestController
 public class CentralCommandController {
 	
-	@Autowired
-	IRadarService radarService;
-
-	@GetMapping("/getLocation")
-	public String getLocation() {
-		return "Coordenadas: " + radarService.getLocation();
-	}
 }
---
+++ src/main/java/com/meli/restservice/service/IDecoderService.java
@@ -0,0 +1,15 @@
+package com.meli.restservice.service;
+
+import com.meli.restservice.dto.SatelliteDTO;
+
+public interface IDecoderService {
+
+	/**
+	 * Descifra mensaje enviado por la nave portacarga a partir de los mensajes
+	 * interceptados por los satelites
+	 * 
+	 * @param satellites que contienen los mensajes captados por cada satelite
+	 * @return {@code String} mensaje descifrado
+	 */
+	String getMessage(SatelliteDTO[] satellites);
+}
